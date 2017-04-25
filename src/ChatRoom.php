2201c76267091624947b276c679e5fd07c07d68c
Noe Rojas <noerojas@csu.fullerton.edu>
2017-04-24 18:40:04 -0700

    update

--- src/ChatRoom.php
+++ src/ChatRoom.php
@@ -7,14 +7,96 @@
   <body>
     <?php include "Navbar.php"; ?>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10">
-          <input class="" type="text" id="chat" >
-          <button id="sendMessage" class="" type="submit">Send</button>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>Final Project - Index Page</title>
+        <?php include "bootstrap.php"; ?>
+        <link href="css/chatroom-style.css" type="text/css" rel="stylesheet">
+      </head>
+      <body>
+        <?php include "navbar.php"; ?>
+
+        <div class="container">
+          <div class="row">
+            <h3 class="page-header">Chat Room: "subject"</h3>
+
+            <div class="col-md-3">
+              <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th>
+                      Connected Users
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <span class="glyphicon glyphicon-ok-sign"></span>&nbsp;
+                      <span class="connected-user">SAMPLE_USER10</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <span class="glyphicon glyphicon-ok-sign"></span>&nbsp;
+                      <span class="connected-user">SAMPLE USER11</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <span class="glyphicon glyphicon-ok-sign"></span>&nbsp;
+                      <span class="connected-user">SAMPLE USER12</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <div class="col-md-8">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  Conversations&nbsp;&nbsp;<span class="glyphicon glyphicon-comment"></span>
+                </div>
+              </div>
+              <div class="panel-body">
+                <ul id="chat" class="chat">
+
+
+                <?php
+
+                  echo "hello worllllrd";
+
+                  $name = $_POST['data'];
+                  //echo $name;
+                ?>
+
+
+                </ul>
+              </div>
+            <div class="panel-footer">
+              <div class="input-group">
+
+                <input id="chat-input" type="text" class="form-control" placeholder="Type your message here..." />
+
+                <span class="input-group-btn">
+                  <button class="btn btn-success" id="sendChat">Send</button>
+                </span>
+
+              </div>
+            </div>
+         </div>
         </div>
-     </div>
-    </div>
+
+
+
+        <?php include "scripts.php"; ?>
+        <script src="js/chatroom.js"></script>
+      </body>
+    </html>
+
+
+
 
 
 
--- src/Scripts.php
+++ src/Scripts.php
@@ -1,2 +1,6 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous">
+</script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
