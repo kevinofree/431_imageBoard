a9aaac376d12ed70a66ff5a17f5a2e22d2d44a0b
Noe Rojas <noerojas@csu.fullerton.edu>
2017-04-26 09:28:10 -0700

    update

--- src/register.php
+++ src/register.php
@@ -7,7 +7,7 @@
   // If user is on the register page do not display the login area in the navbar
   $_SESSION['register_page'] = 'active';
 
-  if(isset($_POST['submit']))
+  if(isset($_POST['register-submit']))
   {
     // Get form data
     $username = $_POST['username'];
@@ -17,21 +17,23 @@
     // New default user
     $status = 0;
 
-    // Call register new user query
-    $new_user = register_user_query($username, $password, $fullname, $status);
+    // Create database query for a newly egistered user
+    $query = register_user_query($username, $password, $fullname, $status);
 
-    // Perform the query to the database
-    $result = mysqli_query($connection, $new_user);
+    // Perform the query on the database
+    $result = mysqli_query($connection, $query);
 
     // Check if the query contained any errors
     if($result)
     {
-        $_SESSION['success_message'] = "You are now registered and can login.";
-        redirect_to("login.php");
+      $_SESSION['success_message'] = "You are now registered and can login.";
+
+      // If successful redirect the client to the login page
+      redirect_to('login.php');
     }
     else
     {
-      echo "<p style='color:red'>Username taken. Try a different one.</p>";
+      $_SESSION['fail_message'] = "The username is already taken. Please try again.";
     }
   }
 ?>
@@ -47,6 +49,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-5 col-lg-offset-3">
+
           <h2 class="text-center">Join GyroChan!</h2>
           <hr>
           <form method="POST">
@@ -59,11 +62,11 @@
               <input type="text" class="form-control" name="username" maxlength="32" required>
             </div>
             <div class="form-group">
-              <label for="passWord">Password</label>
+              <label for="password">Password</label>
               <input type="password" class="form-control" name="password" maxlength="64" required>
             </div>
             <hr>
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" name="register-submit" class="btn btn-primary">Submit</button>
           </form>
         </div>
       </div>
@@ -73,5 +76,6 @@
 </html>
 <?php
   unset($_SESSION['register_page']);
+  unset($_SESSION['fail_message']);
   require_once('./database/close-connection.php');
 ?>
